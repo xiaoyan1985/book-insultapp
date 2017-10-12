29a6de06883df4621f39b8e1dce3847c3ba132f2
xiaoyan1985 <32161110+xiaoyan1985@users.noreply.github.com>
2017-10-12 17:10:07 +0800

    Update InsultGenerator.java

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -11,6 +11,7 @@ public class InsultGenerator {
 		String vowels = "AEIOU";
 		String article = "an";
 		String theInsult = "";
+		
 		try {
 			String databaseURL = "jdbc:postgresql://";
 			databaseURL += System.getenv("POSTGRESQL_SERVICE_HOST");
@@ -18,16 +19,19 @@ public class InsultGenerator {
 			String username = System.getenv("POSTGRESQL_USER");
 			String password = System.getenv("PGPASSWORD");
 			Connection connection = DriverManager.getConnection(databaseURL, username,password);
+			
 			if (connection != null) {
 				String SQL = "select a.string AS first, b.string AS second, c.string AS noun from short_adjective a , long_adjective b, noun c ORDER BY random() limit 1";
 				Statement stmt = connection.createStatement();
 				ResultSet rs = stmt.executeQuery(SQL);
+				
 				while (rs.next()) {
 					if (vowels.indexOf(rs.getString("first").charAt(0)) == -1) {
 						article = "a";
 					}
 					theInsult = String.format("Thou art %s %s %s %s!", article, rs.getString("first"), rs.getString("second"), rs.getString("noun"));
 				}
+				
 				rs.close();
 				connection.close();
 			}
